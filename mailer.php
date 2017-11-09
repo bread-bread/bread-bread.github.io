a2d270b8dcd0192748986fcd0e1ae115679314b3
bread-bread <p.reshetnev@yandex.ru>
2017-11-09 15:05:25 +0300

    correction_imgs

--- mailer.php
+++ mailer.php
@@ -1,8 +1,8 @@
 <?php 
-    header('Content-Type: application/json');
+    header('Content-Type: application/json; charset=utf-8');
     $name = $_POST['name'];
     $message = "$name прислал сообщение";
-    $result = mail('p.reshetnev@yandex.ru', 'Заказ с сайта', $message);
+    $result = mail("p.reshetnev@yandex.ru", "Заказ с сайта", $message);
 
     echo json_encode(array(
         'status' => $result,
